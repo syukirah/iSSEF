2fb933d9d63ba74830cac500ad9a7bdd1e8c61c3
Norashikin <norashikin.yahaya@gmail.com>
2016-02-15 16:13:19 +0800

    Service Management: 1.Create sidebar menu 2.Add my catalog page 3.Add new service page, create form 4. Add mysql :db.js

--- app.js
+++ app.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var path = require('path');
+//var mysql      = require('mysql');
+
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var urlEncodedParser = bodyParser.urlencoded({extended: false});
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+var about = require('./routes/about');//about page
+var catalog = require('./routes/catalog');//catalog page
+var newservice = require('./routes/new-service');//new service page
+
+
+
+/*
+//Initiate mysql connection
+var mysqlConnection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'issefportal'
+});
+
+
+connection  = mysqlConnection.connect(function(err) {
+
+    if ( !err ) {
+        console.log("Connected to Mysql");
+
+
+    }else if ( err ) {
+        console.log(err);
+    }
+
+});*/
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/about', about);//about page
+app.use('/catalog', catalog);//my catalog page
+app.use('/new-service',newservice);//new service page
 app.set('view engine', 'ejs');
 
 
@@ -15,11 +56,7 @@ app.get('/', function(req, res) {
   res.render('pages/index',{ title: 'Test Page - Shikin!' });
 });
 
-// about page
-app.get('/about', function(req, res) {
-  res.render('pages/about');
-});
-
+/*
 // catalog page
 app.get('/catalog', function(req, res) {
     res.render('pages/catalog');
@@ -28,7 +65,7 @@ app.get('/catalog', function(req, res) {
 // new service page
 app.get('/new-service', function(req, res) {
     res.render('pages/new-service');
-});
+});*/
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
