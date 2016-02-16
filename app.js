68d1c7ceb2a0cb09386dad653ee74faacf08abf7
Mohammad Harris <wajacps@gmail.com>
2016-02-16 08:50:53 +0800

    Preparing with Login and Registration page

--- app.js
+++ app.js
@@ -1,24 +1,69 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 var app = express();
 
-
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-// use res.render to load up an ejs view file
-
-// index page
-app.get('/', function(req, res) {
-  res.render('pages/index',{ title: 'Test Page - Shikin!' });
+//////////////////////////
+// Added by Harris
+//////////////////////////
+app.use('/',function(req, res, next) {
+  res.render("index");
 });
 
-// about page
-app.get('/about', function(req, res) {
-  res.render('pages/about');
+app.use('/about',function(req, res, next) {
+  res.render("about");
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.use('/signup',function(req, res, next) {
+  res.render("signup");
 });
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
